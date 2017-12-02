8c7e9cc17aba4a43b12bf9ca8eabd4d3edee0a0a
Yanghao Xie <yhaoxie@gmail.com>
2017-12-02 13:44:56 +0800

    Add init-evil.el

---
+++ lisp/init-evil.el
@@ -0,0 +1,7 @@
+(require 'evil)
+(evil-mode 1)
+;; (loop for (mode . state) in
+;;       '((eshell-mode . emacs)
+;; 	(magit-mode . emacs))
+;;       do (evil-set-initial-state mode state))
+(provide 'init-evil)
