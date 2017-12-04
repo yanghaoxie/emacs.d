7b917f2278b87ba7dd015d96c23d2b976344b00e
Yanghao Xie <yhaoxie@gmail.com>
2017-12-04 15:30:56 +0800

    Change evil scroll to golden ratio scroll

--- lisp/init-golden-ratio-scroll-screen.el
+++ lisp/init-golden-ratio-scroll-screen.el
@@ -1,3 +1,3 @@
-(global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
-(global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up)
+(global-set-key [remap evil-scroll-page-up] 'golden-ratio-scroll-screen-down)
+(global-set-key [remap evil-scroll-page-down] 'golden-ratio-scroll-screen-up)
 (provide 'init-golden-ratio-scroll-screen)
