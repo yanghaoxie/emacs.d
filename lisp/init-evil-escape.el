9f805d2d1be6f4fe224ce6dab5b42cb3537d83d1
Yanghao Xie <yhaoxie@gmail.com>
2017-12-05 21:18:19 +0800

    Decrease evil-escape-delay to 0.1s.

--- lisp/init-evil-escape.el
+++ lisp/init-evil-escape.el
@@ -1,4 +1,4 @@
 (setq-default evil-escape-key-sequence "jk")
 (setq evil-escape-unordered-key-sequence 1)
-(setq-default evil-escape-delay 0.5)
+(setq-default evil-escape-delay 0.1)
 (provide 'init-evil-escape)
