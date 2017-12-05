9b650effff9be034d04b1375e63de71f933ac1c0
Yanghao Xie <yhaoxie@gmail.com>
2017-12-05 21:17:31 +0800

    Auto generated by custome, I donot know what happened.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (airline-cool)))
  '(custom-safe-themes
    (quote
-    ("251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default))))
+    ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default))))
 (provide 'custom)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
