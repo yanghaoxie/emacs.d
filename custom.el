5b4bd700b281971cff42691dd8f224a1a6de19f9
Yanghao Xie <yhaoxie@gmail.com>
2017-12-08 21:28:24 +0800

    Change flycheck error and warning faces

--- custom.el
+++ custom.el
@@ -22,3 +22,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-error ((t (:slant italic))))
+ '(flycheck-warning ((t (:slant italic))))
