daa69f063f3095fc36461c9899c2c826bc973d95
Yanghao Xie <yhaoxie@gmail.com>
2017-12-08 21:23:48 +0800

    Disable elpy elpu-module-flymake

--- custom.el
+++ custom.el
@@ -8,6 +8,9 @@
    (quote
     ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default))))
 (provide 'custom)
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
