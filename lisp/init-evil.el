b98b2ffb4fb0d7b24bd0d86d7c55407e64f0bf0f
Yanghao Xie <yhaoxie@gmail.com>
2017-12-08 21:19:55 +0800

    Set magit initial mode as emacs

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,7 +1,9 @@
 (require 'evil)
+(setq evil-default-state 'normal)
+(loop for (mode . state) in
+      '(
+	(magit-mode . emacs)
+	)
+      do (evil-set-initial-state mode state))
 (evil-mode 1)
-;; (loop for (mode . state) in
-;;       '((eshell-mode . emacs)
-;; 	(magit-mode . emacs))
-;;       do (evil-set-initial-state mode state))
 (provide 'init-evil)
