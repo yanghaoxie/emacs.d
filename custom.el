466f9eb24609c07ed34e1adefa890590fa860ced
Yanghao Xie <yhaoxie@gmail.com>
2017-12-08 21:27:05 +0800

    Auto generated

--- custom.el
+++ custom.el
@@ -6,8 +6,6 @@
  '(custom-enabled-themes (quote (airline-cool)))
  '(custom-safe-themes
    (quote
-    ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default))))
-(provide 'custom)
     ("721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default)))
  '(elpy-modules
    (quote
@@ -24,4 +22,3 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
