78a47d732350eac5246ffe1d461d32e49672b45f
Yanghao Xie <yhaoxie@gmail.com>
2017-12-19 16:45:47 +0800

    Auto truncate lines.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,3 +1,4 @@
 (global-set-key (kbd "C-c o") 
                 (lambda () (interactive) (find-file "~/Dropbox/document/org/main.org")))
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil))) 
 (provide 'init-org)
