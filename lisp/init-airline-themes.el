a28af85a315edac0db6c71ae46173607599abe3e
Yanghao Xie <yhaoxie@gmail.com>
2017-12-19 16:47:59 +0800

    Disable airline change eshell color.

--- lisp/init-airline-themes.el
+++ lisp/init-airline-themes.el
@@ -1,6 +1,7 @@
 (require 'airline-themes)
 ;;(load-theme 'airline-cool) ;; the airline theme is loaded in custom.el
 (setq airline-display-directory 0)
+(setq airline-eshell-colors nil)
 ;; (setq airline-directory-shortened 1)
 ;; (setq airline-shortened-directory-lengh 10)
 ;; eshell pwd is hided by modifying the source code in airline-themes.el
