b5fa8abe97e585643098301e4bc3c316e299a46f
Yanghao Xie <yhaoxie@gmail.com>
2017-12-19 16:53:03 +0800

    Neotree mode.

---
+++ lisp/init-neotree.el
@@ -0,0 +1,6 @@
+;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+(provide 'init-neotree)
