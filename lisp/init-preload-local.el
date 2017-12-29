71f9d4fe153573c9ef0bbc71ec845ed43d7b2034
Yanghao Xie <yhaoxie@gmail.com>
2017-12-29 10:51:15 +0800

    Change margin face, tab width, and yes-or-no-p.

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -3,7 +3,11 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (show-paren-mode 1)
-(set-face-attribute 'fringe nil :background "gray100" :foreground "gray100")
+(set-face-attribute 'fringe nil :background "gray98" :foreground "gray100")
+(setq tab-stop-list (number-sequence 4 200 4))
+(setq tab-width 4)
+;; replace yes and no with y and n
+(defalias 'yes-or-no-p 'y-or-n-p)
 ;; solve company yasnippet conflicts
 (defun company-yasnippet-or-completion ()
   "Solve company yasnippet conflicts."
