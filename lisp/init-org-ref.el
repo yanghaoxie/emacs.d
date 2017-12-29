7edb7a4a2e01cb610f1ed73f3717558746bfc3a3
Yanghao Xie <yhaoxie@gmail.com>
2017-12-29 10:53:37 +0800

    Set default bib file, define a open pdf function.

---
+++ lisp/init-org-ref.el
@@ -0,0 +1,22 @@
+(setq org-ref-default-bibliography '("~/Dropbox/software/Zotero/bibtex/main.bib"))
+;; (setq org-ref-pdf-directory "~/Dropbox/software/Zotero/storage")
+;; (setq bibtex-completion-pdf-open-function
+;;   (lambda (fpath)
+;;     (start-process "open" "*open*" "open" fpath)))
+(setq bibtex-completion-pdf-field "file")
+;; (setq bibtex-completion-pdf-extenaion '(".pdf" ".html"))
+(setq bibtex-completion-find-additional-pdfs t)
+(setq bibtex-completion-bibliography "~/Dropbox/software/Zotero/bibtex/main.bib")
+(defun my/org-ref-open-pdf-at-point ( )
+   "Open the pdf for bibtex key under point if it exists."
+   (interactive)
+   (let* ((results (org-ref-get-bibtex-key-and-file))
+          (key (car results))
+          (pdf-file (car (bibtex-completion-find-pdf key))))
+     (if (file-exists-p pdf-file)
+         (org-open-file pdf-file)
+       (message "No PDF found for %s" key))))
+
+;; (setq org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point)
+(require 'org-ref)
+(provide 'init-org-ref)
