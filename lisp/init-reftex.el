c047b7dfc81611db9ff4ec0d12fad614690f2124
Yanghao Xie <yhaoxie@gmail.com>
2017-12-29 10:52:58 +0800

    Set default bib file.

--- lisp/init-reftex.el
+++ lisp/init-reftex.el
@@ -1,4 +1,6 @@
 (require 'reftex)
+(setq reftex-default-bibliography '("~/Dropbox/software/Zotero/bibtex/main.bib"))
+(setq helm-bibtex-bibliography '("~/Dropbox/software/Zotero/bibtex/main.bib"))
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex) ; with AUCTeX LaTeX mode
 (add-hook 'latex-mode-hook 'turn-on-reftex) ; with Emacs latex mode
 (provide 'init-reftex)
