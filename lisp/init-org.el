4c6745e12c786187ce70c33a08f3a31dab19d64a
Yanghao Xie <yhaoxie@gmail.com>
2017-12-29 10:49:34 +0800

    Solving undefined citation problem when export latex.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,14 @@
 (global-set-key (kbd "C-c o") 
                 (lambda () (interactive) (find-file "~/Dropbox/document/org/main.org")))
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil))) 
+(require 'org)
+(require 'ox-bibtex)
+(setq org-latex-pdf-process
+      '(
+	"xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "bibtex %b"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	))
+(setq org-log-done t)
 (provide 'init-org)
