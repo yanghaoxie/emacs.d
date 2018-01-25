8c242a221723e1e44f100f819a6a35e67190b72a
Yanghao Xie <yhaoxie@gmail.com>
2018-01-25 10:49:57 +0800

    Enable smartparens mode instead of smatparens strict mode.

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,6 +1,6 @@
 (require 'smartparens-config)
 ;; add new pairs
 (sp-pair "<" ">")
-(add-hook 'prog-mode-hook #'smartparens-strict-mode)
-(add-hook 'LaTeX-mode-hook #'smartparens-strict-mode)
+(add-hook 'prog-mode-hook #'smartparens-mode)
+(add-hook 'LaTeX-mode-hook #'smartparens-mode)
 (provide 'init-smartparens)
