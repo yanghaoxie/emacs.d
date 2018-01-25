d5acca63fe5b5c7092b208c0171aab9a64da3990
Yanghao Xie <yhaoxie@gmail.com>
2018-01-25 10:50:38 +0800

    Default nunber label and do not always show.

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -1,6 +1,6 @@
 (global-set-key (kbd "M-[") 'ace-window)
 (setq aw-background nil)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-(setq aw-dispatch-always t)
+;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+;; (setq aw-dispatch-always t)
 ;; (ace-window-display-mode 1)
 (provide 'init-ace-window)
