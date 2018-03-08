2c4c63b10ad6568c652835d4e0fbd14b95f6571a
Yanghao Xie <yhaoxie@gmail.com>
2018-03-08 09:35:59 +0800

    set firefox as browse-url-browser-function to firefox

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-generic-program "firefox")
  '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
