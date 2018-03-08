5ff1b706c4d10303e2899082143b3972c365c784
Yanghao Xie <yhaoxie@gmail.com>
2018-03-08 09:29:03 +0800

    Replace deserted API.

--- lisp/init-elpy.el
+++ lisp/init-elpy.el
@@ -2,5 +2,6 @@
 (setq elpy-rpc-backend "jedi")
 (setq python-shell-interpreter "python3.5")
 (setq elpy-rpc-python-command "python")
-(elpy-use-ipython)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
 (provide 'init-elpy)
