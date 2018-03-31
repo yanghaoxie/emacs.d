154b3965f0e49068cfdde7f2b3c852801f1587ad
yanghaoxie <yhaoxie@gmail.com>
2018-03-31 23:28:16 +0800

    add other package to use-package

--- lisp/init-use-package.el
+++ lisp/init-use-package.el
@@ -3,8 +3,73 @@
 
 (use-package use-package
   :ensure t)
-(use-package yasnippet
+(use-package evil
+    :ensure t)
+(use-package airline-themes
   :ensure t)
+(use-package ace-window
+    :ensure t)
+(use-package anzu
+    :ensure t)
+(use-package auctex
+  :ensure t
+  :pin gnu)
+(use-package avy
+    :ensure t)
+(use-package bash-completion
+    :ensure t)
 (use-package company
-  :ensure t)
-
+    :ensure t)
+(use-package elpy
+    :ensure t)
+(use-package evil-anzu
+    :ensure t)
+(use-package evil-escape
+    :ensure t)
+(use-package evil-nerd-commenter
+    :ensure t)
+(use-package evil-snipe
+    :ensure t)
+(use-package evil-surround
+    :ensure t)
+(use-package expand-region
+    :ensure t)
+(use-package fill-column-indicator
+    :ensure t)
+(use-package flycheck
+    :ensure t)
+(use-package flyspell
+    :ensure t)
+(use-package general
+    :ensure t)
+(use-package golden-ratio-scroll-screen
+    :ensure t)
+(use-package helm
+    :ensure t)
+(use-package highlight-indentation
+    :ensure t)
+(use-package magit
+    :ensure t)
+(use-package neotree
+    :ensure t)
+(use-package org
+    :ensure t)
+(use-package org-ref
+    :ensure t)
+(use-package powerline
+    :ensure t)
+(use-package rainbow-delimiters
+    :ensure t)
+(use-package reftex
+    :ensure t)
+(use-package rich-minority
+    :ensure t)
+(use-package smartparens
+    :ensure t)
+(use-package sudo-edit
+    :ensure t)
+(use-package window-numbering
+    :ensure t)
+(use-package yasnippet
+    :ensure t)
+(provide 'init-use-package)
