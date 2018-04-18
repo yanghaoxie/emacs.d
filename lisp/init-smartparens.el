f4487378b369c70a36082ed63ef0362e64719793
yanghao_xie <yhaoxie@gmail.com>
2018-04-18 14:46:08 +0800

    set sp-local-pair for latex-mode and emacs-lisp-mode

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,7 +1,8 @@
 (require 'smartparens-config)
 (smartparens-global-mode 1)
 ;; add new pairs
-(sp-pair "<" ">")
+;; (sp-pair "<" ">")
+(sp-local-pair '(latex-mode emacs-lisp-mode) "<" ">")
 (add-hook 'prog-mode-hook #'smartparens-mode)
 (add-hook 'LaTeX-mode-hook #'smartparens-mode)
 (provide 'init-smartparens)
