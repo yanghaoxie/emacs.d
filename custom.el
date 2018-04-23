7efc0b52cd84153498ab5820596fbb894d22b730
yanghao_xie <yhaoxie@gmail.com>
2018-04-23 10:45:52 +0800

    set secondary-selection face using custom

--- custom.el
+++ custom.el
@@ -5,5 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-flx zoom zenburn-theme yagist window-numbering use-package uptimes swiper sudo-edit sphinx-doc rich-minority realgud rainbow-delimiters org-ref org-plus-contrib org neotree memoize lua-mode jedi ht helm-swoop helm-gtags golden-ratio-scroll-screen ggtags general fullframe font-lock+ flycheck fill-column-indicator expand-region exec-path-from-shell evil-surround evil-snipe evil-smartparens evil-org evil-nerd-commenter evil-matchit evil-escape evil-anzu eshell-z esh-autosuggest elpy diminish company-quickhelp cdlatex bash-completion auctex airline-themes ace-window ace-jump-buffer 0blayout))))
+    (ranger elpy zoom zenburn-theme yasnippet yagist window-numbering use-package uptimes swiper sudo-edit sphinx-doc rich-minority realgud rainbow-delimiters pyvenv pcomplete-extension org-ref org-plus-contrib org neotree memoize magit lua-mode jedi ht highlight-indentation helm-swoop helm-gtags golden-ratio-scroll-screen ggtags general fullframe font-lock+ flycheck find-file-in-project fill-column-indicator expand-region exec-path-from-shell evil-surround evil-snipe evil-smartparens evil-org evil-nerd-commenter evil-matchit evil-escape evil-anzu eshell-z esh-autosuggest diminish company-quickhelp company-flx color-theme-sanityinc-solarized cdlatex bash-completion auctex airline-themes ace-window ace-jump-buffer 0blayout))))
 (provide 'custom)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(secondary-selection ((t (:background "white smoke")))))
