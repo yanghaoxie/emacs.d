b2415c7a77da517ecabb8846cd5b5b46a7cf5050
yanghao_xie <yhaoxie@gmail.com>
2018-04-27 09:46:53 +0800

    change to python3

--- lisp/init-elpy.el
+++ lisp/init-elpy.el
@@ -1,7 +1,8 @@
 (elpy-enable)
 (setq elpy-rpc-backend "jedi")
-(setq python-shell-interpreter "python3.5")
-(setq elpy-rpc-python-command "python")
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
+;; (setq python-shell-interpreter "ipython3")
+(setq elpy-rpc-python-command "python3")
+(setq python-shell-interpreter "ipython3"
+      python-shell-interpreter-args "-i --simple-prompt"
+      )
 (provide 'init-elpy)
