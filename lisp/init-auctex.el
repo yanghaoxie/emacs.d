f7da99dbb1de56bf9b6b5b318b55c1214ae8adc4
yanghao_xie <yhaoxie@gmail.com>
2018-04-27 09:39:50 +0800

    comment out Tex-view-program

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -4,13 +4,13 @@
 (add-hook 'latex-mode-hook (lambda () (setq truncate-lines nil))) 
 (add-hook 'latex-mode-hook 'turn-on-cdlatex)
 (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
-(setq TeX-view-program-selection
-   (quote
-    (((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "Okular")
-     (output-html "xdg-open"))))
+;; (setq TeX-view-program-selection
+;;    (quote
+;;     (((output-dvi has-no-display-manager)
+;;       "dvi2tty")
+;;      ((output-dvi style-pstricks)
+;;       "dvips and gv")
+;;      (output-dvi "xdvi")
+;;      (output-pdf "Okular")
+;;      (output-html "xdg-open"))))
 (provide 'init-auctex)
