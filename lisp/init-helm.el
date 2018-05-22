37ac8337ab137a332a0498e95e0ba856893a9cd2
yanghao_xie <yhaoxie@gmail.com>
2018-05-22 09:17:55 +0800

    fix sudo completion

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -35,5 +35,13 @@
             (eshell-cmpl-initialize)
             (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete)
 	    ;; (define-key eshell-mode-map (kbd "M-p") 'helm-eshell-history)
+	    ))
+;; fix sudo completion
+(defun pcomplete/sudo ()
+  (let ((prec (pcomplete-arg 'last -1)))
+    (cond ((string= "sudo" prec)
+           (while (pcomplete-here*
+                   (funcall pcomplete-command-completion-function)
+                   (pcomplete-arg 'last) t))))))
 
 (provide 'init-helm)
