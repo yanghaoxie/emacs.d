31d4280ad76a9d7e04f66e4068ce1b01c0acd6a7
yanghao_xie <yhaoxie@gmail.com>
2018-05-28 11:17:56 +0800

    set-face-attribute for whitespace-line

--- lisp/init-whitespace-mode.el
+++ lisp/init-whitespace-mode.el
@@ -3,4 +3,7 @@
 (setq whitespace-line-column 80)
 (setq fill-column 80)
 (setq whitespace-style '(face lines-tail))
+(set-face-attribute 'whitespace-line nil
+		    :background "gray98"
+		    :foreground "violet")
 (provide 'init-whitespace-mode)
