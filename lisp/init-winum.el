07ff6223ab05360a9419c1a1d70cc92fb27f1062
yanghao_xie <yhaoxie@gmail.com>
2018-05-30 09:17:43 +0800

    setq winum-scope as 'frame-local

--- lisp/init-winum.el
+++ lisp/init-winum.el
@@ -2,5 +2,6 @@
 (set-face-attribute 'winum-face nil
 		    :foreground "#8b6508")
 (setq winum-ignored-buffers '("*helm mini*" "*helm M-x*" "*helm find files*"))
+(setq winum-scope 'frame-local)
 
 (provide 'init-winum)
