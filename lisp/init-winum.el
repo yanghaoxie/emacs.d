9b14b73dd36a94ebf9dd94fdd98beaaf6f5a9637
yanghao_xie <yhaoxie@gmail.com>
2018-06-19 21:56:14 +0800

    disable winum-auto-setup-mode-line

--- lisp/init-winum.el
+++ lisp/init-winum.el
@@ -1,7 +1,8 @@
-(winum-mode 1)
+
 (set-face-attribute 'winum-face nil
 		    :foreground "#8b6508")
 (setq winum-ignored-buffers '("*helm mini*" "*helm M-x*" "*helm find files*"))
 (setq winum-scope 'frame-local)
-
+(setq winum-auto-setup-mode-line 'nil)
+(winum-mode 1)
 (provide 'init-winum)
