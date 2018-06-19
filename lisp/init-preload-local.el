c3fafa316a04a83bda606e75e6a4ad5573f9b933
yanghao_xie <yhaoxie@gmail.com>
2018-06-19 21:57:38 +0800

    comment out proxy setting

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -18,4 +18,9 @@
 (set-face-attribute 'secondary-selection nil :background "gray95")
 ;; truncate file name
 ;; (setq-default mode-line-buffer-identification (list -40 (propertized-buffer-identification "%12b")))
+;; proxy
+;; (setq url-gateway-method 'socks)
+;; (setq socks-server '("Default server" "127.0.0.1" 1080 5))
+;; (setq url-gateway-local-host-regexp
+;;       (concat "\\`" (regexp-opt '("localhost" "127.0.0.1")) "\\'"))
 (provide 'init-preload-local)
