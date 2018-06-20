7b6a8ec9a6f2d51fd2325d43363cde5448b09c46
yanghao_xie <yhaoxie@gmail.com>
2018-06-20 10:51:41 +0800

    add init-elfeed.el

---
+++ lisp/init-elfeed.el
@@ -0,0 +1,9 @@
+(elfeed-goodies/setup)
+(setq elfeed-feeds
+      '(
+	("http://nullprogram.com/feed/" blog emacs)
+	("http://nedroid.com/feed/" webcomic)
+	("http://planet.emacsen.org/atom.xml" planet emacs)
+	("https://xkcd.com/rss.xml" xkcd)
+		))
+(provide 'init-elfeed)
