a85087f2e1793ceb28890b278a7c7884b46e4b2b
Yanghao Xie <yhaoxie@gmail.com>
2018-07-11 18:50:03 +0800

    set airline directory display

--- lisp/init-airline-themes.el
+++ lisp/init-airline-themes.el
@@ -2,7 +2,7 @@
 ;;(load-theme 'airline-cool) ;; the airline theme is loaded in custom.el
 (setq airline-display-directory 0)
 (setq airline-eshell-colors nil)
-;; (setq airline-directory-shortened 1)
-;; (setq airline-shortened-directory-lengh 10)
+(setq airline-directory-shortened 1)
+(setq airline-shortened-directory-lengh 10)
 ;; eshell pwd is hided by modifying the source code in airline-themes.el
 (provide 'init-airline-themes)
