2aad7c578b3882d6eaedfb5675aeb75a5fa4bd45
Yanghao Xie <yhaoxie@gmail.com>
2018-07-19 01:33:20 +0800

    disable shell check startup files

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -4,6 +4,7 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
+(setq exec-path-from-shell-check-startup-files nil)
 
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
