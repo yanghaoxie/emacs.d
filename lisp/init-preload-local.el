44c68cadfba7e0d14d636ac33279c1d05fc8adb8
Yanghao Xie <yhaoxie@gmail.com>
2018-07-18 21:33:19 +0800

    enable fringe

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -8,7 +8,7 @@
 '(browse-url-browser-function (quote browse-url-generic))
 '(browse-url-generic-program "firefox")
 (set-face-attribute 'fringe nil :background "gray98" :foreground "gray")
-(set-fringe-mode 0)
+;; (set-fringe-mode 0)
 (setq tab-stop-list (number-sequence 4 200 4))
 (setq tab-width 4)
 ;; push clipboard onto the killring
