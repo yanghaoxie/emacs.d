312e939d4cd63903b3b6c0beff47dd623d1bfd59
Yanghao Xie <yhaoxie@gmail.com>
2018-09-18 21:08:21 +0800

    add init-spaceline.el

---
+++ lisp/init-spaceline.el
@@ -0,0 +1,16 @@
+(use-package spaceline
+    :ensure t
+    :init
+    (progn
+      (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
+	    powerline-default-separator 'wave
+	    anzu-cons-mode-line-p nil
+	    spaceline-minor-modes-p nil
+	    spaceline-buffer-encoding-abbrev-p nil
+	    spaceline-window-numbers-unicode t
+	    spaceline-workspace-numbers-unicode t
+	    powerline-height 18))
+    :config
+    (spaceline-spacemacs-theme)
+    (spaceline-compile))
+(provide 'init-spaceline)
