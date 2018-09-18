7875750ec91061856ac989b43071e105567e727e
Yanghao Xie <yhaoxie@gmail.com>
2018-09-18 09:04:22 +0800

    delete unused code

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -33,46 +33,11 @@
 (add-to-list 'package-archives '("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))
 
 
-;;; Fire up package.el
+;; ;;; Fire up package.el
 
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-;; package.el updates the saved version of package-selected-packages correctly only
-;; after custom-file has been loaded, which is a bug. We work around this by adding
-;; the required packages to package-selected-packages after startup is complete.
-(when (fboundp 'package--save-selected-packages)
-  (require-package 'seq)
-  (add-hook 'after-init-hook
-            (lambda () (package--save-selected-packages
-                   (seq-uniq (append sanityinc/required-packages package-selected-packages))))))
 
-
-(require-package 'fullframe)
-(fullframe list-packages quit-window)
-
-
-(require-package 'cl-lib)
-(require 'cl-lib)
-
-(defun sanityinc/set-tabulated-list-column-width (col-name width)
-  "Set any column with name COL-NAME to the given WIDTH."
-  (when (> width (length col-name))
-    (cl-loop for column across tabulated-list-format
-             when (string= col-name (car column))
-             do (setf (elt column 1) width))))
-
-(defun sanityinc/maybe-widen-package-menu-columns ()
-  "Widen some columns of the package menu table to avoid truncation."
-  (when (boundp 'tabulated-list-format)
-    (sanityinc/set-tabulated-list-column-width "Version" 13)
-    (let ((longest-archive-name (apply 'max (mapcar 'length (mapcar 'car package-archives)))))
-      (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
-
-(add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
-
-;; install use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
 (provide 'init-elpa)
