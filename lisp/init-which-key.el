78ad341eba2523bc06b08a311d60191ba052cff5
Yanghao Xie <yhaoxie@gmail.com>
2018-09-19 11:06:20 +0800

    set which-key-sort-order as alpha

--- lisp/init-which-key.el
+++ lisp/init-which-key.el
@@ -5,5 +5,6 @@
     (which-key-mode)
     :config
     (setq which-key-idle-delay 0.4
-          which-key-idle-secondary-delay 0))
+          which-key-idle-secondary-delay 0
+	        which-key-sort-order 'which-key-key-order-alpha))
 (provide 'init-which-key)
