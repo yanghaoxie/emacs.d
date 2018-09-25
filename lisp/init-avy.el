d08e6f782bf8c8243ee6e824f5e0b7c0d32f1300
Yanghao Xie <yhaoxie@gmail.com>
2018-09-25 17:44:55 +0800

    key chord for avy

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -9,5 +9,10 @@
        "jj" 'evil-avy-goto-char-timer
        "jl" 'evil-avy-goto-line
        "jw" 'evil-avy-goto-word-or-subword-1
-       "jc" 'evil-avy-goto-char-2)))
+       "jc" 'evil-avy-goto-char-2))
+    :chords
+    ("tt" . evil-avy-goto-char-timer)
+    ("ii" . evil-avy-goto-line)
+    ("oo" . evil-avy-goto-word-or-subword-1)
+    ("cc" . evil-avy-goto-char-2))
 (provide 'init-avy)
