17b2ead792f1285d4bbedffbb9116ba6377c623b
Yanghao Xie <yhaoxie@gmail.com>
2018-09-25 15:49:42 +0800

    change doom-modeline file name style to file-name

--- lisp/init-doom-modeline.el
+++ lisp/init-doom-modeline.el
@@ -1,5 +1,7 @@
 (use-package doom-modeline
   :ensure t
   :defer t
+  :config
+  (setq doom-modeline-buffer-file-name-style 'file-name)
   :hook (after-init . doom-modeline-init))
 (provide 'init-doom-modeline)
