cb2a987055b5d24c06747ad1ddf1cde7d96340a3
Yanghao Xie <yhaoxie@gmail.com>
2018-09-26 15:05:52 +0800

    do not show compile-log buffer

--- lisp/init-auto-compile.el
+++ lisp/init-auto-compile.el
@@ -2,5 +2,8 @@
     :ensure t
     :config
     (auto-compile-on-load-mode)
-    (auto-compile-on-save-mode))
+    (auto-compile-on-save-mode)
+    (setq load-prefer-newer t
+	  auto-compile-display-buffer nil
+	  auto-compile-mode-line-counter t))
 (provide 'init-auto-compile)
