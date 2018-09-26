91124df6d04f2350f9d086d3cf1db0ae8d4b9610
Yanghao Xie <yhaoxie@gmail.com>
2018-09-26 20:17:51 +0800

    key binding for my/byte-compile-init-dir

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,6 +1,7 @@
 (my/leader-keys-major-mode
  :keymaps 'emacs-lisp-mode-map
  "cc" 'emacs-lisp-byte-compile
+ "ci"  'my/byte-compile-init-dir
  "eb" 'eval-buffer
  "ee" 'eval-last-sexp
  "er" 'eval-region
