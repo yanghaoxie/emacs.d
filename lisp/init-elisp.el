ee9cd50aa7fb8625c11d408be275e6c724755034
Yanghao Xie <yhaoxie@gmail.com>
2018-09-27 16:40:33 +0800

    indent buffer

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,9 +1,9 @@
 (my/leader-keys-major-mode
- :keymaps 'emacs-lisp-mode-map
- "cc" 'emacs-lisp-byte-compile
- "ci"  'my/byte-compile-init-dir
- "eb" 'eval-buffer
- "ee" 'eval-last-sexp
- "er" 'eval-region
- "ef" 'eval-defun)
+  :keymaps 'emacs-lisp-mode-map
+  "cc" 'emacs-lisp-byte-compile
+  "ci"  'my/byte-compile-init-dir
+  "eb" 'eval-buffer
+  "ee" 'eval-last-sexp
+  "er" 'eval-region
+  "ef" 'eval-defun)
 (provide 'init-elisp)
