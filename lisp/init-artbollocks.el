1007d300b0813b483526077f793dbdad04040ee4
Yanghao Xie <yhaoxie@gmail.com>
2018-09-27 15:54:12 +0800

    do not check for jargon and passive voice

--- lisp/init-artbollocks.el
+++ lisp/init-artbollocks.el
@@ -4,5 +4,8 @@
   :defer t
   :init
   (add-hook 'org-mode-hook 'artbollocks-mode)
-  (add-hook 'LaTeX-mode-hook 'artbollocks-mode))
+  (add-hook 'LaTeX-mode-hook 'artbollocks-mode)
+  :config
+  (setq artbollocks-jargon nil
+	artbollocks-passive-voice nil))
 (provide 'init-artbollocks)
