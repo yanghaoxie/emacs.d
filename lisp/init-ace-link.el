8dfb235b9502fe742e0582eec65de840adb95c6d
Yanghao Xie <yhaoxie@gmail.com>
2018-10-08 19:25:23 +0800

    indent buffer

--- lisp/init-ace-link.el
+++ lisp/init-ace-link.el
@@ -2,11 +2,11 @@
   :ensure t
   :init
   (progn
-      (with-eval-after-load 'info
-        (define-key Info-mode-map "o" 'ace-link-info))
-      (with-eval-after-load 'help-mode
-        (define-key help-mode-map "o" 'ace-link-help))
-      (with-eval-after-load 'eww
-        (define-key eww-link-keymap "o" 'ace-link-eww)
-        (define-key eww-mode-map "o" 'ace-link-eww))))
+    (with-eval-after-load 'info
+      (define-key Info-mode-map "o" 'ace-link-info))
+    (with-eval-after-load 'help-mode
+      (define-key help-mode-map "o" 'ace-link-help))
+    (with-eval-after-load 'eww
+      (define-key eww-link-keymap "o" 'ace-link-eww)
+      (define-key eww-mode-map "o" 'ace-link-eww))))
 (provide 'init-ace-link)
