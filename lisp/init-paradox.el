eac153dccaab24a7f19663aabab5a1dc50cce13c
Yanghao Xie <yhaoxie@gmail.com>
2018-10-10 23:12:45 +0800

    key bindings for paradox-menu-mode

--- lisp/init-paradox.el
+++ lisp/init-paradox.el
@@ -4,5 +4,14 @@
   :init
   (progn
     (my/leader-keys
-     "ak" 'paradox-list-packages)))
+      "ak" 'paradox-list-packages))
+  :general
+  (:keymaps 'paradox-menu-mode-map
+	    :states 'normal
+	    "i" 'package-menu-mark-install
+	    "d" 'package-menu-mark-delete
+	    "U" 'package-menu-mark-upgrades
+	    "u" 'package-menu-mark-unmark
+	    "x" 'package-menu-execute
+	    "q" 'paradox-quit-and-close))
 (provide 'init-paradox)
