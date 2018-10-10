832e163c0109415bb559c94de0395f55c280a9b1
Yanghao Xie <yhaoxie@gmail.com>
2018-10-10 10:00:43 +0800

    use ivy as synosaurus-choose-method

--- lisp/init-synosaurus.el
+++ lisp/init-synosaurus.el
@@ -6,5 +6,6 @@
     "xSl" 'synosaurus-lookup
     "xSr" 'synosaurus-choose-and-replace)
   :config
-  (setq synosaurus-backend 'synosaurus-backend-wordnet))
+  (setq synosaurus-backend 'synosaurus-backend-wordnet
+	synosaurus-choose-method 'ivy-read))
 (provide 'init-synosaurus)
