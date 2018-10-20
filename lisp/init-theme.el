14be9ab9f2b3794ebb3aae2c70d2dcb1bc763d16
Yanghao Xie <yhaoxie@gmail.com>
2018-10-20 17:02:28 +0800

    use gruvbox dark and light themes

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,17 +1,15 @@
-(use-package darktooth-theme
-    :ensure t
-    :init
-    (load-theme 'darktooth t))
-
-(use-package spacemacs-theme
+(use-package gruvbox-theme
   :ensure t
-  :defer t)
-
+  :defer t
+  :init
+  (load-theme 'gruvbox-light-soft t)
+  )
 ;;;###autoload
 (defun my/switch-theme ()
   (interactive)
-  (my/toggle-next-theme 'darktooth 'spacemacs-light))
+  (my/toggle-next-theme 'gruvbox-dark-soft 'gruvbox-light-soft))
 
 (my/leader-keys
   "Tn" 'my/switch-theme)
+
 (provide 'init-theme)
