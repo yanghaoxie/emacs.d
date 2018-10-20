d10adb62f1408fe8b2d9d3ab5ccf6004cce47aad
Yanghao Xie <yhaoxie@gmail.com>
2018-10-20 16:27:33 +0800

    set initial state of calendar-mode as normal

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -8,6 +8,7 @@
     (evil-set-initial-state 'pdf-view-mode 'normal)
     (evil-set-initial-state 'pdf-outline-buffer-mode 'normal)
     (evil-set-initial-state 'paradox-menu-mode 'normal)
+    (evil-set-initial-state 'calendar-mode 'normal)
     (setq evil-insert-state-cursor '(bar "LimeGreen")
           evil-normal-state-cursor '(box "darkorange")
 	  evil-visual-state-cursor '(box "LightGoldenrod")
