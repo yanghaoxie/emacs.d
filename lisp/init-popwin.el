a2e4a57ee5fa5d1117f14ffc36d0b093509ef463
Yanghao Xie <yhaoxie@gmail.com>
2018-10-23 15:08:20 +0800

    use popwin to manage *PDF-Occur* buffers

--- lisp/init-popwin.el
+++ lisp/init-popwin.el
@@ -26,5 +26,6 @@
       (push '("*Synonyms List*"     :dedicated t :position bottom :stick t :noselect nil   :height 0.4) popwin:special-display-config)
       (push '("*Ledger Report*"     :dedicated t :position right :stick t :noselect nil   :width 0.6) popwin:special-display-config)
       (push '("\*Outline.*pdf\*"     :regexp t :dedicated t :position right :stick t :noselect nil   :width 0.3) popwin:special-display-config)
+      (push '("*PDF-Occur*"     :dedicated t :position right :stick t :noselect nil   :width 0.4) popwin:special-display-config)
       ))
 (provide 'init-popwin)
