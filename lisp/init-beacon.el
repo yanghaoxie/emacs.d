ed0eeb238b1311755c5a53bdc31c6029313370e0
Yanghao Xie <yhaoxie@gmail.com>
2018-10-30 09:59:20 +0800

    set beacon-size to 10

--- lisp/init-beacon.el
+++ lisp/init-beacon.el
@@ -3,7 +3,8 @@
   :diminish beacon-mode
   :config
   (setq beacon-blink-when-window-scrolls nil
-	beacon-dont-blink-major-modes '(t pdf-view-mode))
+	beacon-dont-blink-major-modes '(t pdf-view-mode)
+	beacon-size 10)
   (beacon-mode 1)
   )
 (provide 'init-beacon)
