4c95b27b14f5412b9e042cee7be60d1333ff42b0
Yanghao Xie <yhaoxie@gmail.com>
2018-11-07 10:52:41 +0800

    rebind "jc" to evil-avy-goto-char

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -9,5 +9,5 @@
        "jj" 'evil-avy-goto-char-timer
        "jl" 'evil-avy-goto-line
        "jw" 'evil-avy-goto-word-or-subword-1
-       "jc" 'evil-avy-goto-char-2)))
+       "jc" 'evil-avy-goto-char)))
 (provide 'init-avy)
