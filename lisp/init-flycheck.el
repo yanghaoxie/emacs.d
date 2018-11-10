68c8342f6b2bc5f2313abae03c291c48e09f493b
Yanghao Xie <yhaoxie@gmail.com>
2018-11-10 09:33:33 +0800

    adopt spacemacs flycheck fringe face, which add different color of dots at the fringe

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -18,7 +18,53 @@
     ;; navigation
     "en" 'flycheck-next-error
     "ep" 'flycheck-previous-error
-    "el" 'flycheck-list-errors))
+    "el" 'flycheck-list-errors)
+  (progn
+      ;; Custom fringe indicator
+      (when (and (fboundp 'define-fringe-bitmap)
+                 ;; (not syntax-checking-use-original-bitmaps)
+		 )
+        (define-fringe-bitmap 'my-flycheck-fringe-indicator
+          (vector #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00011100
+                  #b00111110
+                  #b00111110
+                  #b00111110
+                  #b00011100
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000
+                  #b00000000)))
+      (let ((bitmap 'my-flycheck-fringe-indicator
+	     ;; (if syntax-checking-use-original-bitmaps
+             ;;            'flycheck-fringe-bitmap-double-arrow
+             ;;   'my-flycheck-fringe-indicator)
+	     ))
+        (flycheck-define-error-level 'error
+          :severity 2
+          :overlay-category 'flycheck-error-overlay
+          :fringe-bitmap bitmap
+          :error-list-face 'flycheck-error-list-error
+          :fringe-face 'flycheck-fringe-error)
+        (flycheck-define-error-level 'warning
+          :severity 1
+          :overlay-category 'flycheck-warning-overlay
+          :fringe-bitmap bitmap
+          :error-list-face 'flycheck-error-list-warning
+          :fringe-face 'flycheck-fringe-warning)
+        (flycheck-define-error-level 'info
+          :severity 0
+          :overlay-category 'flycheck-info-overlay
+          :fringe-bitmap bitmap
+          :error-list-face 'flycheck-error-list-info
+          :fringe-face 'flycheck-fringe-info))))
 
 (use-package flycheck-posframe
   :if (version<= "26.1" emacs-version)
