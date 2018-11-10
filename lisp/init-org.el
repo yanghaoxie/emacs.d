44af4a7f7c5f04b396f233f5dd7b2a9c19caecd2
Yanghao Xie <yhaoxie@gmail.com>
2018-11-10 16:13:56 +0800

    indent buffer

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -7,25 +7,24 @@
     (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
     (setq org-directory "~/Dropbox/document/org")
     (setq org-agenda-files (apply 'append
-                                  (mapcar
-                                   (lambda (directory)
-                                     (directory-files-recursively
-                                      directory org-agenda-file-regexp))
-                                   '("~/Dropbox/document/org/" ))))
+				  (mapcar
+				   (lambda (directory)
+				     (directory-files-recursively
+				      directory org-agenda-file-regexp))
+				   '("~/Dropbox/document/org/" ))))
     (setq org-latex-pdf-process
-          '(
-            "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-            "bibtex %b"
-            "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-            "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-            ))
-    (setq org-log-done t)
+	  '(
+	    "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	    "bibtex %b"
+	    "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	    "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	    ))
     (with-eval-after-load 'ox-latex
       (add-to-list 'org-latex-classes
-                   '("IEEEtran" "\\documentclass{IEEEtran}"
-                     ("\\section{%s}" . "\\section*{%s}")
-                     ("\\subsection{%s}" . "\\subsection*{%s}")
-                     ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+		   '("IEEEtran" "\\documentclass{IEEEtran}"
+		     ("\\section{%s}" . "\\section*{%s}")
+		     ("\\subsection{%s}" . "\\subsection*{%s}")
+		     ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
     ;; latex highlight in org mode
     (setq org-highlight-latex-and-related '(latex script entities))
 
