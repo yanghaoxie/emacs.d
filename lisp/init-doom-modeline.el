6fdfb730a5ac8b03272cde5d91d561069bf4c921
Yanghao Xie <yhaoxie@gmail.com>
2018-11-22 20:42:18 +0800

    set doom modeline height to 12 and bar width to 1

--- lisp/init-doom-modeline.el
+++ lisp/init-doom-modeline.el
@@ -2,6 +2,8 @@
   :ensure t
   :defer t
   :config
-  (setq doom-modeline-buffer-file-name-style 'file-name)
+  (setq doom-modeline-buffer-file-name-style 'file-name
+	doom-modeline-height 12
+	doom-modeline-bar-width 1)
   :hook (after-init . doom-modeline-init))
 (provide 'init-doom-modeline)
