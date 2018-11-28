385d89396db5cb2df57420e3d5d05180c9027793
Yanghao Xie <yhaoxie@gmail.com>
2018-11-28 16:04:21 +0800

    functions and keybindings for avy to search over all windows

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -7,7 +7,36 @@
             avy-background t)
       (my/leader-keys
        "jj" 'avy-goto-char-timer
+       "j M-j" 'my/avy-goto-char-timer-all-windows
        "jl" 'avy-goto-line
+       "j M-l" 'my/avy-goto-line-all-windows
        "jw" 'avy-goto-word-or-subword-1
-       "jc" 'avy-goto-char)))
+       "j M-w" 'my/avy-goto-word-or-subword-1-all-windows
+       "jc" 'avy-goto-char
+       "j M-c" 'my/avy-goto-char-all-windows)))
+
+;;;###autoload
+(defun my/avy-goto-char-timer-all-windows ()
+  (interactive)
+  (let ((avy-all-windows t))
+    (avy-goto-char-timer)))
+
+;;;###autoload
+(defun my/avy-goto-line-all-windows ()
+  (interactive)
+  (let ((avy-all-windows t))
+    (avy-goto-line)))
+
+;;;###autoload
+(defun my/avy-goto-word-or-subword-1-all-windows ()
+  (interactive)
+  (let ((avy-all-windows t))
+    (avy-goto-word-or-subword-1)))
+
+;;;###autoload
+(defun my/avy-goto-char-all-windows ()
+  (interactive)
+  (let ((avy-all-windows t))
+    (call-interactively 'avy-goto-char)))
+
 (provide 'init-avy)
