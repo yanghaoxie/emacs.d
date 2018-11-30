f7aeffb8e2f351b1eaedb5f88d7290ec6fd92e38
Yanghao Xie <yhaoxie@gmail.com>
2018-11-30 15:53:43 +0800

    enable savehist mode, use savehist to save python shell command history

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -131,6 +131,48 @@
     "wU" 'winner-redo)
   :config
   (winner-mode))
+
+(use-package savehist
+  :defer 5
+  :config
+  (progn
+    (setq savehist-autosave-interval 10)
+    (savehist-mode 1)
+    ;; save shell history https://oleksandrmanzyuk.wordpress.com/2011/10/23/a-persistent-command-history-in-emacs/
+    (defun comint-write-history-on-exit (process event)
+      "Write comint history of PROCESS when EVENT happened to a file specified in buffer local var 'comint-input-ring-file-name' (defined in turn-on-comint-history)."
+      (comint-write-input-ring)
+      (let ((buf (process-buffer process)))
+	(when (buffer-live-p buf)
+	  (with-current-buffer buf
+	    (insert (format "\nProcess %s %s" process event))))))
+    (defun turn-on-comint-history ()
+      "Setup comint history.
+When comint process started set buffer local var
+'comint-input-ring-file-name', so that a file name is specified to write
+and read from comint history.
+
+That 'comint-input-ring-file-name' is buffer local is determined by the
+4th argument to 'add-hook' below.  And localness is important, because
+otherwise 'comint-write-input-ring' will find mentioned var nil."
+      (let ((process (get-buffer-process (current-buffer))))
+	(when process
+	  (setq comint-input-ring-file-name
+		(format "~/.emacs.d/inferior-%s-history"
+			(process-name process)))
+	  (comint-read-input-ring)
+	  (set-process-sentinel process
+				#'comint-write-history-on-exit))))
+    (defun mapc-buffers (fn)
+      (mapc (lambda (buffer)
+	      (with-current-buffer buffer
+		(funcall fn)))
+	    (buffer-list)))
+    (defun comint-write-input-ring-all-buffers ()
+      (mapc-buffers 'comint-write-input-ring))
+    (add-hook 'inferior-python-mode-hook 'turn-on-comint-history nil nil)
+    (add-hook 'kill-buffer-hook 'comint-write-input-ring)
+    (add-hook 'kill-emacs-hook 'comint-write-input-ring-all-buffers)))
 ;; key bindings
 
 (my/leader-keys
