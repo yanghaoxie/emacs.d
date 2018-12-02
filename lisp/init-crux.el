bec926193f481275758bb2222e3d459f8754053f
Yanghao Xie <yhaoxie@gmail.com>
2018-12-02 15:57:07 +0800

    make "C-a" goto beginning of line in any evil states

--- lisp/init-crux.el
+++ lisp/init-crux.el
@@ -2,10 +2,12 @@
   :ensure t
   :config
   (progn
-    (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+    (my/all-states-keys
+      "C-a" 'crux-move-beginning-of-line)
     (my/leader-keys
       "f M-r" 'crux-rename-file-and-buffer
       "f M-o" 'crux-open-with
       "fi" 'crux-find-user-init-file)
     (crux-with-region-or-buffer indent-region)))
+
 (provide 'init-crux)
