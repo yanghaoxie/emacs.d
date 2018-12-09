0305bc207e2bf634a9decc585d825e650276fa29
Yanghao Xie <yhaoxie@gmail.com>
2018-12-09 09:32:50 +0800

    add use-package evil-indent-plus

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -87,4 +87,20 @@
   :defer t
   :config
   (evil-goggles-mode))
+
+(use-package evil-indent-plus
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (define-key evil-inner-text-objects-map "i" 'evil-indent-plus-i-indent)
+    (define-key evil-outer-text-objects-map "i" 'evil-indent-plus-a-indent)
+    (define-key evil-inner-text-objects-map "I" 'evil-indent-plus-i-indent-up)
+    (define-key evil-outer-text-objects-map "I" 'evil-indent-plus-a-indent-up)
+    (define-key evil-inner-text-objects-map "J"
+      'evil-indent-plus-i-indent-up-down)
+    (define-key evil-outer-text-objects-map "J"
+      'evil-indent-plus-a-indent-up-down)))
+
 (provide 'init-evil)
+
