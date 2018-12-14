a797ebb3a4e7f6b98885984b3ed4a4a398298557
Yanghao Xie <yhaoxie@gmail.com>
2018-12-14 20:55:01 +0800

    move setq to config block

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -1,12 +1,13 @@
 (use-package undo-tree
   :defer t
   :diminish undo-tree-mode
-  :init (setq undo-tree-visualizer-timestamps t
-              undo-tree-visualizer-diff t)
   :commands undo-tree-visualize
+  :init
   (progn
     (my/leader-keys
       "au" 'undo-tree-visualize))
   :config
   (progn
+    (setq undo-tree-visualizer-timestamps t
+	  undo-tree-visualizer-diff t)))
 (provide 'init-undo-tree)
