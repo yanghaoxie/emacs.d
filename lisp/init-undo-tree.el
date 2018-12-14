051bfaf819c6956f03ab09e3b16c0617805f313e
Yanghao Xie <yhaoxie@gmail.com>
2018-12-14 20:54:30 +0800

    delete code from spacemacs, default evil-motion-state is good

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -9,16 +9,4 @@
       "au" 'undo-tree-visualize))
   :config
   (progn
-    ;; restore diff window after quit.  TODO fix upstream
-    (defun spacemacs/undo-tree-restore-default ()
-      (setq undo-tree-visualizer-diff t))
-    ;; (spacemacs|hide-lighter undo-tree-mode)
-    ;; (evilified-state-evilify-map undo-tree-visualizer-mode-map
-    ;;   :mode undo-tree-visualizer-mode
-    ;;   :bindings
-    ;;   (kbd "j") 'undo-tree-visualize-redo
-    ;;   (kbd "k") 'undo-tree-visualize-undo
-    ;;   (kbd "h") 'undo-tree-visualize-switch-branch-left
-    ;; (kbd "l") 'undo-tree-visualize-switch-branch-right)
-    ))
 (provide 'init-undo-tree)
