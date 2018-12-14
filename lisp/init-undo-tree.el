d125f27e1c243e24c0042123711c6be9dcaa7cea
Yanghao Xie <yhaoxie@gmail.com>
2018-12-14 20:53:43 +0800

    use keyword commands to defer undo-tree

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -3,6 +3,7 @@
   :diminish undo-tree-mode
   :init (setq undo-tree-visualizer-timestamps t
               undo-tree-visualizer-diff t)
+  :commands undo-tree-visualize
   (progn
     (my/leader-keys
       "au" 'undo-tree-visualize))
