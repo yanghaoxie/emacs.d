0ae22def86c2077fb106395dbf72a3816cbc351c
Yanghao Xie <yhaoxie@gmail.com>
2018-12-24 17:39:00 +0800

    format function

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -1,22 +1,22 @@
 (use-package avy
-    :ensure t
-    :defer t
-    :init
-    (progn
-      (setq avy-all-windows nil
-            avy-background t)
-      (my/leader-keys
-       "jj" 'avy-goto-char-timer
-       "j M-j" 'my/avy-goto-char-timer-all-windows
-       "jl" 'avy-goto-line
-       "j M-l" 'my/avy-goto-line-all-windows
-       "jw" 'avy-goto-word-or-subword-1
-       "j M-w" 'my/avy-goto-word-or-subword-1-all-windows
-       "jc" 'avy-goto-char
-       "j M-c" 'my/avy-goto-char-all-windows))
-    (my/leader-keys-major-mode
-      :keymaps 'org-mode-map
-      "jj" 'avy-org-goto-heading-timer))
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (setq avy-all-windows nil
+	  avy-background t)
+    (my/leader-keys
+      "jj" 'avy-goto-char-timer
+      "j M-j" 'my/avy-goto-char-timer-all-windows
+      "jl" 'avy-goto-line
+      "j M-l" 'my/avy-goto-line-all-windows
+      "jw" 'avy-goto-word-or-subword-1
+      "j M-w" 'my/avy-goto-word-or-subword-1-all-windows
+      "jc" 'avy-goto-char
+      "j M-c" 'my/avy-goto-char-all-windows))
+  (my/leader-keys-major-mode
+    :keymaps 'org-mode-map
+    "jj" 'avy-org-goto-heading-timer))
 
 ;;;###autoload
 (defun my/avy-goto-char-timer-all-windows ()
