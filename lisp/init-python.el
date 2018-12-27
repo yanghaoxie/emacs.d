639849e319046d2c69eb8ec2126e9f6afe95a6fc
Yanghao Xie <yhaoxie@gmail.com>
2018-12-27 18:43:20 +0800

    move anaconda related config to init-python-anaconda.el

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -23,35 +23,6 @@
       :keymaps 'inferior-python-mode-map
       "M-o" 'ace-link)))
 
-(use-package anaconda-mode
-  :ensure t
-  :diminish anaconda-mode
-  :defer t
-  :after python
-  :init
-  (progn
-    (my/leader-keys-major-mode
-      :keymaps 'python-mode-map
-      :major-modes t
-      "h" '(:ignore t :which-key "help")
-      "hh" 'anaconda-mode-show-doc
-      "g" '(:ignore t :which-key "goto")
-      "ga" 'anaconda-mode-find-assignments
-      "gb" 'anaconda-mode-go-back
-      "gu" 'anaconda-mode-find-references
-      "gd" 'anaconda-mode-find-definitions
-      "gD" 'anaconda-mode-find-definitions-other-window)
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
-
-(use-package company-anaconda
-  :ensure t
-  :defer t
-  :after 'anaconda-mode
-  :init
-  (progn
-    (add-to-list 'company-backend 'company-anaconda)))
-
 (use-package yapfify
   :ensure t
   :diminish yapf-mode
