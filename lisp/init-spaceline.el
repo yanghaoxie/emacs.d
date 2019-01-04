4d317dc138e819e42471fe35ff2711167e151e76
Yanghao Xie <yhaoxie@gmail.com>
2019-01-04 18:37:35 +0800

    format block

--- lisp/init-spaceline.el
+++ lisp/init-spaceline.el
@@ -1,19 +1,18 @@
 (use-package spaceline
-    :ensure t
-    :pin melpa-stable
-    :init
-    (progn
-      (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
-	    powerline-default-separator 'slant
-	    anzu-cons-mode-line-p nil
-	    spaceline-minor-modes-p nil
-	    spaceline-purpose-p t
-	    spaceline-purpose-hide-if-not-dedicated t
-	    spaceline-buffer-encoding-abbrev-p nil
-	    spaceline-window-numbers-unicode nil
-	    spaceline-workspace-numbers-unicode nil
-	    spaceline-input-method-p nil))
-    :config
-    (spaceline-spacemacs-theme)
-    (spaceline-compile))
+  :ensure t
+  :pin melpa-stable
+  :init
+  (progn
+    (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
+	  powerline-default-separator 'slant
+	  spaceline-minor-modes-p nil
+	  spaceline-purpose-p t
+	  spaceline-purpose-hide-if-not-dedicated t
+	  spaceline-buffer-encoding-abbrev-p nil
+	  spaceline-window-numbers-unicode nil
+	  spaceline-workspace-numbers-unicode nil
+	  spaceline-input-method-p nil))
+  :config
+  (spaceline-spacemacs-theme)
+  (spaceline-compile))
 (provide 'init-spaceline)
