5a7ea22717729d21c47c5e25b5e1d593f5b62038
Yanghao Xie <yhaoxie@gmail.com>
2019-01-04 18:39:46 +0800

    comment out spaceline configuration block since we are using spaceline-all-the-icons

--- lisp/init-spaceline.el
+++ lisp/init-spaceline.el
@@ -1,17 +1,21 @@
 (use-package spaceline
   :ensure t
   :pin melpa-stable
-  :init
-  (progn
-    (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
-	  powerline-default-separator 'slant
-	  spaceline-minor-modes-p nil
-	  spaceline-purpose-p t
-	  spaceline-purpose-hide-if-not-dedicated t
-	  spaceline-buffer-encoding-abbrev-p nil
-	  spaceline-window-numbers-unicode nil
-	  spaceline-workspace-numbers-unicode nil
-	  spaceline-input-method-p nil))
+  ;; :init
+  ;; (progn
+  ;;   (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
+  ;; 	  powerline-default-separator 'slant
+  ;; 	  spaceline-minor-modes-p nil
+  ;; 	  spaceline-purpose-p t
+  ;; 	  spaceline-purpose-hide-if-not-dedicated t
+  ;; 	  spaceline-buffer-encoding-abbrev-p nil
+  ;; 	  spaceline-window-numbers-unicode nil
+  ;; 	  spaceline-workspace-numbers-unicode nil
+  ;; 	  spaceline-input-method-p nil))
+  ;; :config
+  ;; (spaceline-spacemacs-theme)
+  ;; (spaceline-compile)
+  )
   :config
   (spaceline-spacemacs-theme)
   (spaceline-compile))
