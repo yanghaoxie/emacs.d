a3a1937bfb03742dc2a08d09a36adb775b8bdb0a
Yanghao Xie <yhaoxie@gmail.com>
2019-01-10 20:34:17 +0800

    fix bug that reset value of undo-tree-visualizer-diff, which is copied from spacemacs

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -11,6 +11,9 @@
     (setq undo-tree-visualizer-timestamps t
 	  undo-tree-visualizer-diff t
 	  undo-tree-auto-save-history t
-	  undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))))
+	  undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+    (defun spacemacs/undo-tree-restore-default ()
+      (setq undo-tree-visualizer-diff t))
+    (advice-add 'undo-tree-visualizer-quit :after #'spacemacs/undo-tree-restore-default)))
 
 (provide 'init-undo-tree)
