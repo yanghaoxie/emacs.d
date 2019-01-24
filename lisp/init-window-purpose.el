2af0c505ba3c8e3a120db9a09ee0441f0c933f8f
Yanghao Xie <yhaoxie@gmail.com>
2019-01-24 15:27:36 +0800

    format block

--- lisp/init-window-purpose.el
+++ lisp/init-window-purpose.el
@@ -4,13 +4,13 @@
   :init
   (progn
     (my/leader-keys
-     "rb" 'purpose-switch-buffer-with-purpose
-     "rB" 'switch-buffer-without-purpose
-     "rd" 'purpose-toggle-window-purpose-dedicated
-     "r M-d" 'purpose-toggle-window-buffer-dedicated
-     "rD" 'purpose-delete-non-dedicated-windows
-     "rp" 'purpose-switch-buffer-with-some-purpose
-     "rP" 'purpose-set-window-purpose))
+      "rb" 'purpose-switch-buffer-with-purpose
+      "rB" 'switch-buffer-without-purpose
+      "rd" 'purpose-toggle-window-purpose-dedicated
+      "r M-d" 'purpose-toggle-window-buffer-dedicated
+      "rD" 'purpose-delete-non-dedicated-windows
+      "rp" 'purpose-switch-buffer-with-some-purpose
+      "rP" 'purpose-set-window-purpose))
   :config
   (add-to-list 'purpose-user-mode-purposes '(python-mode . py))
   (add-to-list 'purpose-user-mode-purposes '(inferior-python-mode . py-repl))
