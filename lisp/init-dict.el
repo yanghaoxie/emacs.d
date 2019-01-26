9590964953a0709397d4e3bcc0efac023d9a3178
Yanghao Xie <yhaoxie@gmail.com>
2019-01-26 18:25:33 +0800

    trying bing dict

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -1,11 +1,11 @@
-(use-package bing-dict
-  :ensure t
-  :defer t
-  :init
-  (progn
-    (setq bing-dict-cache-auto-save nil)
-    (my/leader-keys
-      "aw" 'bing-dict-brief)))
+;; (use-package bing-dict
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (progn
+;;     (setq bing-dict-cache-auto-save nil)
+;;     (my/leader-keys
+;;       "aw" 'bing-dict-brief)))
 
 (use-package youdao-dictionary
   :ensure t
