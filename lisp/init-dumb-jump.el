be2048a193a5bef2c4b0da7f5d5eaa15100f9b79
Yanghao Xie <yhaoxie@gmail.com>
2019-02-01 19:15:12 +0800

    use my/all-states-keys to bind keys for dumb-jump

--- lisp/init-dumb-jump.el
+++ lisp/init-dumb-jump.el
@@ -1,11 +1,12 @@
 (use-package dumb-jump
   :ensure t
-  :bind (("M-g o" . dumb-jump-go-other-window)
-	 ("M-g g" . dumb-jump-go)
-	 ("M-g b" . dumb-jump-back)
-	 ("M-g l" . dumb-jump-quick-look)
-	 ("M-g x" . dumb-jump-go-prefer-external)
-	 ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :init
+  (my/all-states-keys
+    "M-g o" 'dumb-jump-go-other-window
+    "M-g g" 'dumb-jump-go
+    "M-g l" 'dumb-jump-quick-look
+    "M-g x" 'dumb-jump-go-prefer-external
+    "M-g z" 'dumb-jump-go-prefer-external-other-window)
   :config
   (progn
     (setq dumb-jump-selector 'helm)))
