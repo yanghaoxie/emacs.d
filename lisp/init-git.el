92118cfafa3895dc787e37c974c950a0a6ddb658
Yanghao Xie <yhaoxie@gmail.com>
2019-02-12 19:19:34 +0800

    add use-package magit-todos

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -17,4 +17,16 @@
 (use-package evil-magit
     :ensure t
     :after magit)
+
+(use-package magit-todos
+  :ensure t
+  :after magit
+  :general
+  (:keymaps 'magit-todos-section-map
+	    "j" 'evil-next-visual-line)
+  (:keymaps 'magit-status-mode-map
+	    "gT" 'magit-todos-jump-to-todos)
+  :config
+  (magit-todos-mode 1))
+
 (provide 'init-git)
