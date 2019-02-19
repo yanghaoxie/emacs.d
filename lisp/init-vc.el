0098cbecf54cc2e08efa2d18aef955b4d2ecf7cc
Yanghao Xie <yhaoxie@gmail.com>
2019-02-19 20:19:01 +0800

    enable diff-hl-mode, diff-hl-flydiff-mode and diff-hl-margin-mino-mode in emacs-lisp-mode

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -4,6 +4,9 @@
   :init
   (progn
     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-    (add-hook 'emacs-lisp-mode-hook 'diff-hl-mode)))
+    (add-hook 'emacs-lisp-mode-hook (lambda ()
+				      (diff-hl-mode 1)
+				      (diff-hl-flydiff-mode 1)
+				      (diff-hl-margin-minor-mode 1)))))
 
 (provide 'init-vc)
