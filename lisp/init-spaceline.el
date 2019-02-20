ff1473b791054f357b51766eeb5cc0cc67c5b05c
Yanghao Xie <yhaoxie@gmail.com>
2019-02-20 21:01:48 +0800

    delete wrong setq varialbes

--- lisp/init-spaceline.el
+++ lisp/init-spaceline.el
@@ -4,13 +4,9 @@
   (progn
     (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state
 	  powerline-default-separator 'slant
-	  spaceline-minor-modes-p nil
-	  spaceline-purpose-p t
 	  spaceline-purpose-hide-if-not-dedicated t
-	  spaceline-buffer-encoding-abbrev-p nil
 	  spaceline-window-numbers-unicode nil
-	  spaceline-workspace-numbers-unicode nil
-	  spaceline-major-mode-p nil))
+	  spaceline-workspace-numbers-unicode nil))
   :config
   (spaceline-compile)
   (spaceline-helm-mode t)
